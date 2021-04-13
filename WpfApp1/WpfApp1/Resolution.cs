07792368a8240d49677fd215e3ad2fa63d9ffbe2
Byron <byronlong@gmail.com>
2021-04-12 20:48:22 -0400

    creating wpf dotnetcore app

---
+++ WpfApp1/WpfApp1/Resolution.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfApp1
+{
+    public class Resolution
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Height}_{Width}";
+        }
+    }
+}

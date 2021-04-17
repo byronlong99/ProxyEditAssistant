c01dddc15d3beeac95a4e55e41f1baeef6b904f1
Byron <byronlong@gmail.com>
2021-04-16 21:44:23 -0400

    creating ui

--- ProxyEditAssistant/Logic/OptionBuilder.cs
+++ ProxyEditAssistant/Logic/OptionBuilder.cs
@@ -6,23 +6,29 @@ namespace ProxyEditAssistant.Logic
 {
     public class OptionBuilder
     {
-        private readonly List<OptionModel> _options;
-        
+        private List<OptionModel> _options;
         private readonly List<Resolution> _resolutions;
 
         public OptionBuilder(List<Resolution> resolutions)
         {
             _resolutions = resolutions;
-            _options = new List<OptionModel>();
         }
-        
+
         public List<OptionModel> BuildOptions()
         {
-            _options.Add(new OptionModel { ResolutionColor = "#b6ffff", ResolutionText = "360p", Color = "#90caf9", BuildButtonText = "true", Resolution = new Resolution { Height = 360, Width = 360}});
-            _options.Add(new OptionModel { ResolutionColor = "#b6ffff", ResolutionText = "480p", Color = "#90caf9", BuildButtonText = "true", Resolution = new Resolution { Height = 480, Width = 480}});
-            _options.Add(new OptionModel { ResolutionColor = "#b6ffff",  ResolutionText = "720p", Color = "#90caf9", BuildButtonText = "true", Resolution = new Resolution { Height = 480, Width = 720}});
-            _options.Add(new OptionModel { ResolutionColor = "#b6ffff",  ResolutionText = "1080p", Color = "#90caf9", BuildButtonText = "true", Resolution = new Resolution { Height = 480, Width = 1080}});
+            _options = new List<OptionModel>();
 
+            foreach (var resolution in _resolutions)
+            {
+                var optionModel = new OptionModel();
+                optionModel.ResolutionColor = "#b6ffff";
+                optionModel.ResolutionText = $"{resolution.Width.ToString()}p";
+                optionModel.Color = "#90caf9";
+                optionModel.BuildButtonText = "true";
+                optionModel.Resolution = resolution;
+                _options.Add(optionModel);
+            }
+            
             return _options;
         }
     }

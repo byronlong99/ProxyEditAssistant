1535a71e7177a61e13a7e160c2f7d0b59a2dba36
Byron <byronlong@gmail.com>
2021-06-17 13:42:17 -0400

    cleaning up

--- ProxyEditAssistant.Tests/Logic/FileListBuilderTests.cs
+++ ProxyEditAssistant.Tests/Logic/FileListBuilderTests.cs
@@ -11,13 +11,13 @@ namespace ProxyEditAssistant.Tests.Logic
         public void BuildListOfFiles_NoFilesExist_AllFilesAreReturned()
         {
             var resolution = new Resolution {Height = 360, Width = 240};
-            
+
             var fileListBuilder = new FileListBuilder();
 
             var files = fileListBuilder.BuildListOfFiles(resolution, "TestVideos");
-            
+
             Assert.AreEqual(4, files.Count);
-            
+
             Assert.AreEqual(@"TestVideos\Source\Folder 1\DJI_0215.MP4", files[0]);
             Assert.AreEqual(@"TestVideos\Source\Folder 1\DJI_0216.MP4", files[1]);
             Assert.AreEqual(@"TestVideos\Source\Folder 2\DJI_0218.MP4", files[2]);
